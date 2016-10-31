585490e63b25ee28140d0678dff607b74a51cbef
ashwinchidambaram1 <ashwinchidambaram1@gmail.com>
2016-10-31 01:20:47 -0400

    unit 6 wrap up pt.4 fixed integer limit from 4 to 8

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates(:email, presence: true, length: { maximum: 30 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false })
 	validates(:typeofperson, presence: true, length: { maximum: 10 })
-	validates(:phone, presence: true, length: { maximum: 11 }, uniqueness: true)
+	validates(:phone, presence: true, uniqueness: true)
 	validates(:housenum, presence: true, length: { maximum: 5 })
 	validates(:street, presence: true, length: { maximum: 15 })
 	validates(:town, presence: true, length: { maximum: 20 })
