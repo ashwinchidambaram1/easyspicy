1b978ff1b304c0c58d5c46637065e3f6b001d98e
ashwinchidambaram1 <ashwinchidambaram1@gmail.com>
2016-11-02 13:49:50 -0400

    removed payment method

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161102163706) do
+ActiveRecord::Schema.define(version: 20161102174904) do
 
   create_table "users", force: :cascade do |t|
     t.string   "fname"
@@ -25,7 +25,6 @@ ActiveRecord::Schema.define(version: 20161102163706) do
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
     t.string   "password_digest"
-    t.string   "paymentmethod"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
