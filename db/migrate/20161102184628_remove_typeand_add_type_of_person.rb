7c01354e7d3b8819494887619116cab2846c15ab
ashwinchidambaram1 <ashwinchidambaram1@gmail.com>
2016-11-02 16:30:55 -0400

    try again

--- db/migrate/20161102184628_remove_typeand_add_type_of_person.rb
+++ db/migrate/20161102184628_remove_typeand_add_type_of_person.rb
@@ -1,6 +1,5 @@
 class RemoveTypeandAddTypeOfPerson < ActiveRecord::Migration[5.0]
   def change
-  	remove_column :users, :type
   	add_column :users, :member, :string
   	add_column :users, :payType, :string
 
