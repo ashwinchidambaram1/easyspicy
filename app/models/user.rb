18a7588ab28fe57fd9497417c0715d4fa54c05e0
ashwinchidambaram1 <ashwinchidambaram1@gmail.com>
2016-11-08 23:36:23 -0500

    signup try 1

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,13 @@ class User < ApplicationRecord
 	validates(:lname, presence: true, length: { maximum: 15 })
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates(:email, presence: true, length: { maximum: 30 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false })
-	#validates(:type, presence: true, length: { maximum: 10 })
 	validates(:phone, presence: true, uniqueness: true)
-	validates(:housenum, presence: true, length: { maximum: 5 })
-	validates(:street, presence: true, length: { maximum: 15 })
-	validates(:town, presence: true, length: { maximum: 20 })
-	validates(:state, presence: true, length: { maximum: 20})
+	validates(:housenum, length: { maximum: 5 })
+	validates(:street, length: { maximum: 15 })
+	validates(:town, length: { maximum: 20 })
+	validates(:state, length: { maximum: 20})
 	validates(:member, presence: true, length: { maximum: 20})
-	validates(:payType, presence:true, length: { maximum: 20} )
+	validates(:payType, length: { maximum: 20} )
 	has_secure_password
 	validates :password, presence: true, length: { minimum: 6 }
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
+  resources :users
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
 end
