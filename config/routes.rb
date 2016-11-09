29380067b44027834344a0c46eeb81e654519684
ashwinchidambaram1 <ashwinchidambaram1@gmail.com>
2016-11-09 12:10:50 -0500

    smallererer edit

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   #get 'users/new'
 
   #get 'welcome/index'
-  match ':controller(/:action(/:id))', :via => :get
+  #match ':controller(/:action(/:id))', :via => :get
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
