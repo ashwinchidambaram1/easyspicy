199de0d3f793ee42bcf76d3d5f408b6a92fb6294
ashwinchidambaram1 <ashwinchidambaram1@gmail.com>
2017-02-08 14:47:50 -0500

    changes spacing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,25 @@
 class UsersController < ApplicationController
 	def show
     @user = User.find(params[:id])
-  	end
-  	def new
-  		@user = User.new
-  	end
-    def create
-        @user = User.new(user_params)
-        if @user.save
-            flash[:success] = "Welcome to the Easy Spicy!"
-            redirect_to @user
-        else
-            render 'new'
-        end
-    end
+  end
+  	
+  def new
+  	@user = User.new
+  end
     
-    private 
+  def create
+      @user = User.new(user_params)
+      if @user.save
+          flash[:success] = "Welcome to the Easy Spicy!"
+          redirect_to @user
+      else
+          render 'new'
+      end
+  end
     
-        def user_params
-            params.require(:user).permit(:fname, :lname, :email, :phone, :member, :password, :password_confirmation)
-        end
+  private 
+    
+      def user_params
+          params.require(:user).permit(:fname, :lname, :email, :phone, :member, :password, :password_confirmation)
+      end
 end
