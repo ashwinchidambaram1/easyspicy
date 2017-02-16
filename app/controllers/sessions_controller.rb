91c27e4c87d12d4afcd633efd4b1fb461a72f932
ashwinchidambaram1 <ashwinchidambaram1@gmail.com>
2017-02-16 17:53:32 -0500

    added alert

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
-      puts 'hi'
+      puts '<script> alert("hi") </script>'
       log_in user
       redirect_to user
     
