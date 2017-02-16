88f8f4e2e170410bc923957cb4b79eda12618cd3
ashwinchidambaram1 <ashwinchidambaram1@gmail.com>
2017-02-15 22:56:07 -0500

    bunch of login stuff

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,22 @@
 module SessionsHelper
+	
+	def log_in(user)
+    	session[:user_id] = user.id
+  	end
+
+  	def current_user
+    	@current_user ||= User.find_by(id: session[:user_id])
+  	end
+
+
+  	# Returns true if the user is logged in, false otherwise.
+  	def logged_in?
+    	!current_user.nil?
+  	end
+
+
+  	<%= link_to "Users", stub %>
+  	<%= link_to "Settings", stub %>
+	
+
 end
