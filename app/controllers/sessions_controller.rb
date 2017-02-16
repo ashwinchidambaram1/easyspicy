a7792cd0e2b16ceb6abca3568a8d5a2f01e6cc11
ashwinchidambaram1 <ashwinchidambaram1@gmail.com>
2017-02-15 23:23:19 -0500

    login recoverey 3

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,14 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    #user = User.find_by(email: params[:session][:email].downcase)
+    #if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
-      log_in user
-      redirect_to user
-    else
+      #log_in user
+      #redirect_to user
+    #else
       # Create an error message.
-      # flash.now[:danger] = 'Invalid email/password combination'
+      #flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
   end
 
